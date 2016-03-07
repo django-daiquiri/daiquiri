31f9a4bf9af217264397c1fbde0a5f86b406afe9
Jochen Klar <jklar@aip.de>
2016-03-07 17:43:26 +0100

    login view and form

--- daiquiri_auth/views.py
+++ daiquiri_auth/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login(request):
+    form = LoginForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.login(request)
+            if user:
+                auth_login(request, user)
+                if request.POST.get('next'):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                else:
+                    return HttpResponseRedirect('/')
+
+    return render(request, 'auth/login.html', {'form': form})
