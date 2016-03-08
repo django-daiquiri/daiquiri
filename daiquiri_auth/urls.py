8674368c559270efc70f88d5fe36cf58330ad0c9
Jochen Klar <jklar@aip.de>
2016-03-08 17:45:24 +0100

    change password workflow

--- daiquiri_auth/urls.py
+++ daiquiri_auth/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^update-profile/$', 'daiquiri_auth.views.profile_update', name='profile_update'),
+
+    # change password
+    url(r'^password/change/$', auth_views.password_change, {
+        'template_name': 'auth/password_change_form.html'
+        }, name='password_change'),
+    url(r'^password/change/done/$', auth_views.password_change_done, {
+        'template_name': 'auth/password_change_done.html'
+        }, name='password_change_done'),
 ]
