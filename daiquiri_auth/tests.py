649c2c954aac1f532ae87bf96b4a8ad4c27b8a84
Jochen Klar <jklar@aip.de>
2016-03-21 16:36:33 +0100

    testing mixins and first auth tests added

--- daiquiri_auth/tests.py
+++ daiquiri_auth/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from daiquiri_core.tests import TestListViewMixin
+from daiquiri_core.tests import TestRetrieveViewMixin
+from daiquiri_core.tests import TestCreateViewMixin
+from daiquiri_core.tests import TestUpdateViewMixin
+from daiquiri_core.tests import TestDeleteViewMixin
+from daiquiri_core.tests import TestModelStringMixin
+
+from .models import Profile
+
+
+class AuthTestCase(TestCase):
+    fixtures = ['auth/testing.json']
+
+
+class ProfileTests(TestListViewMixin,
+                   TestModelStringMixin,
+                   AuthTestCase):
+
+    list_url_name = 'users'
+
+    def setUp(self):
+        self.client.login(username='admin', password='admin')
+        self.instance = Profile.objects.get(user__username='user')
