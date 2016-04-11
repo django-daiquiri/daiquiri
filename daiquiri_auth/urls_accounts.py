114aaf5cd7a2ee0898f715b227df7db160660780
Jochen Klar <jklar@aip.de>
2016-04-11 16:06:47 +0200

    add accounts_profile

---
+++ daiquiri_auth/urls_accounts.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+
+from rest_framework import routers
+
+from .views import *
+
+router = routers.DefaultRouter()
+router.register(r'profiles', ProfileViewSet)
+
+urlpatterns = [
+    url(r'^profile/', profile_update, name='account_profile'),
+    url(r'^accounts/', include('allauth.urls')),
+]
