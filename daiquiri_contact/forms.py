2e83704acfd97a384cbff1e434978ac79debfdbd
agalkin <agalkin@aip.de>
2016-10-25 14:22:45 +0200

    more changes on the contact, not ready

--- daiquiri_contact/forms.py
+++ daiquiri_contact/forms.py
@@ -2,8 +2,16 @@ from django import forms
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
+    subject = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
     content = forms.CharField(
         required=True,
         widget=forms.Textarea
     )
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['contact_name'].label = "Your name:"
+        self.fields['contact_email'].label = "Your email:"
+        self.fields['contact_subject'].label = "Subject:"
+        self.fields['contact_message'].label = "Message:"
--- daiquiri_contact/views.py
+++ daiquiri_contact/views.py
@@ -1,12 +1,54 @@
 from django.shortcuts import render
 
-from .forms import ContactForm
+from django.core.mail import EmailMessage
+from django.core.mail import send_mail, BadHeaderError
+from django.shortcuts import redirect
+from django.template import Context
+from django.template.loader import get_template
+from django.http import HttpResponse, HttpResponseRedirect
 
+from forms import ContactForm
 
-# add to your views
 def contact(request):
     form_class = ContactForm
 
-    return render(request, 'contact.html', {
+    # new logic!
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', '')
+            contact_subject = request.POST.get('contact_subject', '')
+            contact_email = request.POST.get('contact_email', '')
+            contact_message = request.POST.get('content', '')
+
+            # Email the profile with the
+            # contact information
+            template = get_template('contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'contact_subject': contact_subject,
+                'contact_message': contact_message,
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" +'',
+                ['youremail@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('contact/thanks')
+
+    return render(request, 'contact/contact.html', {
         'form': form_class,
     })
+
+
+def thanks(request):
+    return HttpResponse('Thank you for your message! We will respond as soon as possible.')
