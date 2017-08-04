eec34b1840ecd4e57c0fd8d23a03ec3cf5a23388
Jochen Klar <jklar@aip.de>
2017-08-04 12:32:40 +0200

    add update email

--- daiquiri/auth/forms.py
+++ daiquiri/auth/forms.py
@@ -9,7 +9,7 @@ from .models import Profile
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name')
 
 
 class ProfileForm(forms.ModelForm):
