cdfb99c179c7fa17d7c34a8e66bbf59490fb7311
Jochen Klar <jklar@aip.de>
2017-08-07 11:46:21 +0200

    add logging to query tasks

--- daiquiri/query/tasks.py
+++ daiquiri/query/tasks.py
@@ -7,8 +7,13 @@ from django.utils.timezone import now
 
 @shared_task
 def run_query(job_id):
+    import logging
+
     from daiquiri.core.adapter import get_adapter
     from daiquiri.query.models import QueryJob
+
+    # get logger
+    logger = logging.getLogger(__name__)
 
     # get the job object from the database
     job = QueryJob.objects.get(pk=job_id)
@@ -35,6 +40,8 @@ def run_query(job_id):
     job.start_time = now()
     job.save()
 
+    logger.info('run_query %s started' % job.id)
+
     # get the actual query and submit the job to the database
     try:
         # this is where the work ist done (and the time is spend)
@@ -43,6 +50,7 @@ def run_query(job_id):
     except (ProgrammingError, InternalError) as e:
         job.phase = job.PHASE_ERROR
         job.error_summary = str(e)
+        logger.info('run_query %s failed (%s)' % (job.id, job.error_summary))
 
     except OperationalError as e:
         # load the job again and check if the job was killed
@@ -51,10 +59,12 @@ def run_query(job_id):
         if job.phase != job.PHASE_ABORTED:
             job.phase = job.PHASE_ERROR
             job.error_summary = str(e)
+            logger.info('run_query %s failed (%s)' % (job.id, job.error_summary))
 
     else:
         # get additional information about the completed job
         job.phase = job.PHASE_COMPLETED
+        logger.info('run_query %s completed' % job.id)
 
     finally:
         # get timing and save the job object
@@ -74,8 +84,16 @@ def run_query(job_id):
 
 @shared_task(track_started=True)
 def create_download_file(file_name, format_key, database_name, table_name, metadata, status, empty):
+    import logging
+
     from daiquiri.core.adapter import get_adapter
+
+    # get logger
+    logger = logging.getLogger(__name__)
+    logger.info('create_download_file %s started' % file_name)
 
     with open(file_name, 'w') as f:
         for line in get_adapter().download.generate(format_key, database_name, table_name, metadata, status, empty):
             f.write(line)
+
+    logger.info('create_download_file %s completed' % file_name)
