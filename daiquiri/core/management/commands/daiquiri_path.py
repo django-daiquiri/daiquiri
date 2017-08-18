5db86f0ede5c95f8b08ebb8c5f536e8d431662cb
Jochen Klar <jklar@aip.de>
2017-08-18 12:47:24 +0200

    fix daiquiri_path script

--- daiquiri/core/management/commands/daiquiri_path.py
+++ daiquiri/core/management/commands/daiquiri_path.py
@@ -5,5 +5,8 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
+    requires_system_checks = False
+    can_import_settings = False
+
     def handle(self, *args, **options):
         print(imp.find_module('daiquiri')[1])
