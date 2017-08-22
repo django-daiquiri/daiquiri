548566b566dbd2fa054ca95754e036bfac58b262
Jochen Klar <jklar@aip.de>
2017-08-22 14:05:40 +0200

    refactor sqlcreate script

--- daiquiri/core/management/commands/sqlcreate.py
+++ daiquiri/core/management/commands/sqlcreate.py
@@ -2,15 +2,15 @@ import socket
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db.utils import OperationalError, ProgrammingError
-
-from daiquiri.metadata.models import Database
 
 
 class Command(BaseCommand):
 
     requires_system_checks = False
     can_import_settings = True
+
+    def add_arguments(self, parser):
+        parser.add_argument('database', nargs='*', help='List of additional databases.')
 
     def get_config(self, key):
         config = settings.DATABASES.get(key)
@@ -51,13 +51,10 @@ CREATE USER \'%(USER)s\'@\'%(CLIENT)s\' identified by \'%(PASSWORD)s\';
 GRANT ALL PRIVILEGES ON `%(PREFIX)s%%`.* to \'%(USER)s\'@\'%(CLIENT)s\';
 ''' % data)
 
-        try:
-            for database in Database.objects.all():
-                data.update({'DATABASE_NAME': database.name})
+        if options['database']:
+            for database_name in options['database']:
+                data.update({'DATABASE_NAME': database_name})
                 print('GRANT SELECT ON `%(DATABASE_NAME)s`.* to \'%(USER)s\'@\'%(CLIENT)s\';' % data)
-        except (OperationalError, ProgrammingError):
-            pass
-        else:
             print('')
 
         print('''-- For testing, run the following commands on \'%(HOST)s\':
