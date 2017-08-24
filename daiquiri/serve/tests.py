57c817c6e932071e6a32424f9d470253fd82cedf
Jochen Klar <jklar@aip.de>
2017-08-24 12:44:07 +0200

    add tests for serve app

--- daiquiri/serve/tests.py
+++ daiquiri/serve/tests.py
@@ -1,3 +1,125 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from test_generator.viewsets import TestListViewsetMixin
+
+from daiquiri.core.tests import TestCase
+
+
+class ServeTestCase(TestCase):
+
+    fixtures = (
+        'auth.json',
+        'metadata.json'
+    )
+
+    users = (
+        ('admin', 'admin'),
+        ('user', 'user'),
+        ('anonymous', None),
+    )
+
+    status_map = {
+        'list_view': {
+            'admin': 200, 'user': 403, 'anonymous': 302
+        },
+        'list_viewset': {
+            'admin': 200, 'user': 403, 'anonymous': 403
+        },
+        'retrieve_viewset': {
+            'admin': 200, 'user': 403, 'anonymous': 403
+        },
+        'create_viewset': {
+            'admin': 201, 'user': 403, 'anonymous': 403
+        },
+        'update_viewset': {
+            'admin': 200, 'user': 403, 'anonymous': 403
+        },
+        'delete_viewset': {
+            'admin': 204, 'user': 403, 'anonymous': 403
+        },
+        'discover_viewset': {
+            'admin': 200, 'user': 403, 'anonymous': 403
+        },
+        'management_viewset': {
+            'admin': 200, 'user': 403, 'anonymous': 403
+        },
+        'user_viewset': {
+            'admin': 200, 'user': 200, 'anonymous': 200
+        }
+    }
+
+
+class PublicRowTests(TestListViewsetMixin, ServeTestCase):
+
+    status_map = {
+        'list_viewset': {
+            'admin': 200, 'user': 200, 'anonymous': 200
+        }
+    }
+
+    url_names = {
+        'viewset': 'serve:row'
+    }
+
+    def get_list_viewset_query_params(self):
+        return {
+            'database': 'daiquiri_data_obs',
+            'table': 'stars'
+        }
+
+
+class InternalRowTests(TestListViewsetMixin, ServeTestCase):
+
+    status_map = {
+        'list_viewset': {
+            'admin': 200, 'user': 200, 'anonymous': 404
+        }
+    }
+
+    url_names = {
+        'viewset': 'serve:row'
+    }
+
+    def get_list_viewset_query_params(self):
+        return {
+            'database': 'daiquiri_data_sim',
+            'table': 'particles'
+        }
+
+
+class PublicColumnTests(TestListViewsetMixin, ServeTestCase):
+
+    status_map = {
+        'list_viewset': {
+            'admin': 200, 'user': 200, 'anonymous': 200
+        }
+    }
+
+    url_names = {
+        'viewset': 'serve:column'
+    }
+
+    def get_list_viewset_query_params(self):
+        return {
+            'database': 'daiquiri_data_obs',
+            'table': 'stars'
+        }
+
+
+class InternalColumnTests(TestListViewsetMixin, ServeTestCase):
+
+    status_map = {
+        'list_viewset': {
+            'admin': 200, 'user': 200, 'anonymous': 404
+        }
+    }
+
+    url_names = {
+        'viewset': 'serve:column'
+    }
+
+    def get_list_viewset_query_params(self):
+        return {
+            'database': 'daiquiri_data_sim',
+            'table': 'particles'
+        }
--- testing/runtests.py
+++ testing/runtests.py
@@ -7,14 +7,11 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-test_labels = [
-    'daiquiri.auth',
-    'daiquiri.core'
-]
-
 
 def main():
     parser = argparse.ArgumentParser(description='Run the tests for Daiquiri.')
+    parser.add_argument('test_label', nargs='*', help='Module paths to test; can be modulename, modulename.TestCase or modulename.TestCase.test_method')
+
     parser.add_argument('-k', '--keepdb', action='store_true', help='Preserves the test DB between runs.')
 
     args = parser.parse_args()
@@ -23,7 +20,7 @@ def main():
 
     django.setup()
     TestRunner = get_runner(settings)
-    failures = TestRunner(verbosity=1, keepdb=args.keepdb).run_tests(test_labels)
+    failures = TestRunner(verbosity=1, keepdb=args.keepdb).run_tests(args.test_label)
     sys.exit(bool(failures))
 
 
