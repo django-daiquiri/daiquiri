47777b2a7a02eff34f9de2909b38f1e86be4f34b
Jochen Klar <jklar@aip.de>
2017-09-01 17:03:26 +0200

    update NOTICE

--- daiquiri/__init__.py
+++ daiquiri/__init__.py
@@ -3,6 +3,6 @@ __version__ = '0.1.0'
 __author__ = 'Jochen Klar'
 __email__ = 'jklar@aip.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2017 Leibniz Institute for Astrophysics Potsdam (AIP)'
+__copyright__ = 'Copyright 2016-2017 Leibniz Institute for Astrophysics Potsdam (AIP)'
 
 VERSION = __version__
