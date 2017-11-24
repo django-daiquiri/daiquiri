2976ef3e0369c72ae9d84d53b0a6692fc7a41a44
Jochen Klar <jklar@aip.de>
2017-11-24 16:42:20 +0100

    refactor cutout django app

--- daiquiri/cutout/views.py
+++ daiquiri/cutout/views.py
@@ -5,16 +5,16 @@ from django.views.generic import TemplateView
 from .utils import get_adapter
 
 
-class CutoutView(AccessMixin, TemplateView):
-    template_name = 'cutout/cutout.html'
+class DatacubeView(AccessMixin, TemplateView):
+    template_name = 'cutout/datacube.html'
 
     def dispatch(self, request, *args, **kwargs):
         if not settings.CUTOUT_ANONYMOUS and not request.user.is_authenticated:
             return self.handle_no_permission()
-        return super(CutoutView, self).dispatch(request, *args, **kwargs)
+        return super(DatacubeView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super(CutoutView, self).get_context_data(**kwargs)
+        context = super(DatacubeView, self).get_context_data(**kwargs)
         context['defaults'] = get_adapter().defaults
 
         return context
--- daiquiri/cutout/viewsets.py
+++ daiquiri/cutout/viewsets.py
@@ -3,7 +3,6 @@ from django.utils.timezone import now
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.decorators import list_route
 
 from daiquiri.core.utils import get_client_ip
 from daiquiri.stats.models import Record
@@ -12,7 +11,7 @@ from .utils import get_adapter
 from .permissions import HasPermission
 
 
-class CutoutViewSet(viewsets.GenericViewSet):
+class DatacubeViewSet(viewsets.GenericViewSet):
     permission_classes = (HasPermission, )
     authentication_classes = (SessionAuthentication, TokenAuthentication)
 
@@ -20,19 +19,16 @@ class CutoutViewSet(viewsets.GenericViewSet):
         adapter = get_adapter()
         adapter.clean(request)
 
-        # create a stats record for this job
-        Record.objects.create(
-            time=now(),
-            resource_type='CUTOUT',
-            resource=adapter.__dict__,
-            client_ip=get_client_ip(request),
-            user=request.user
-        )
+        if request.GET.get('download', True):
+            # create a stats record for this job
+            Record.objects.create(
+                time=now(),
+                resource_type='CUTOUT',
+                resource=adapter.__dict__,
+                client_ip=get_client_ip(request),
+                user=request.user
+            )
 
-        return adapter.perform_cutout(request)
-
-    @list_route(methods=['get'])
-    def validate(self, request):
-        adapter = get_adapter()
-        adapter.clean(request)
-        return Response()
+            return adapter.perform_cutout(request)
+        else:
+            return Response()
