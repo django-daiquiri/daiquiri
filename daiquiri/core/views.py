97825915dd58bf97c32d870039ea9096d274db35
Jochen Klar <jklar@aip.de>
2018-02-21 11:17:24 +0100

    Fix error handler status codes

--- daiquiri/core/views.py
+++ daiquiri/core/views.py
@@ -18,19 +18,19 @@ def home(request):
 
 
 def bad_request(request):
-    return render(request, 'core/400.html')
+    return render(request, 'core/400.html', status=400)
 
 
 def forbidden(request):
-    return render(request, 'core/403.html')
+    return render(request, 'core/403.html', status=403)
 
 
 def not_found(request):
-    return render(request, 'core/404.html')
+    return render(request, 'core/404.html', status=404)
 
 
 def internal_server_error(request):
-    return render(request, 'core/500.html')
+    return render(request, 'core/500.html', status=500)
 
 
 class PermissionRedirectMixin(object):
--- testing/config/urls.py
+++ testing/config/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.i18n import javascript_catalog
 
 from daiquiri.core.views import home
 
@@ -21,3 +20,8 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+handler400 = 'daiquiri.core.views.bad_request'
+handler403 = 'daiquiri.core.views.forbidden'
+handler404 = 'daiquiri.core.views.not_found'
+handler500 = 'daiquiri.core.views.internal_server_error'
