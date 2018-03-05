6f7da9360fdccf1e27a5ff07f2bcde7bd1f5b0ac
Kristin Riebe <kriebe@aip.de>
2018-03-05 14:27:10 +0100

    Settings attribute PARTICIPANT_ACCEPTED_DEFAULT renamed to MEETINGS_PARTICIPANT_ACCEPTED_DEFAULT

--- daiquiri/meetings/forms.py
+++ daiquiri/meetings/forms.py
@@ -33,8 +33,8 @@ class ParticipantForm(forms.ModelForm):
         self.instance.meeting = self.meeting
         self.instance.registered = now()
 
-        if hasattr(settings, 'PARTICIPANT_ACCEPTED_DEFAULT'):
-            self.instance.accepted = settings.PARTICIPANT_ACCEPTED_DEFAULT
+        if hasattr(settings, 'MEETINGS_PARTICIPANT_ACCEPTED_DEFAULT'):
+            self.instance.accepted = settings.MEETINGS_PARTICIPANT_ACCEPTED_DEFAULT
 
         # create an empty details dict if it does not exist
         if not self.instance.details:
