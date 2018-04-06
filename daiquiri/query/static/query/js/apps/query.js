7bef47cba4a168e7a8774e08d8872ca7d9f4d87b
Jochen Klar <jklar@aip.de>
2018-04-06 13:04:26 +0200

    Fix submitting indicator, again

--- daiquiri/query/static/query/js/apps/query.js
+++ daiquiri/query/static/query/js/apps/query.js
@@ -3,7 +3,7 @@ var app = angular.module('query', ['core']);
 app.directive('submitting', ['$timeout', function ($timeout) {
     return {
         restrict: 'E',
-        template: '<i class="fa fa-circle-o-notch fa-spin fa-fw"></i>',
+        template: '<i class="daiquiri-query-submitting fa fa-circle-o-notch fa-spin fa-fw"></i>',
         link: function (scope, element, attrs) {
             scope.timeout = null;
             scope.isActive = function () {
@@ -13,12 +13,12 @@ app.directive('submitting', ['$timeout', function ($timeout) {
                 if (value) {
                     if (scope.timeout === null) {
                         scope.timeout = $timeout(function(){
-                            element.removeClass('ng-hide');
+                            $('.daiquiri-query-submitting').removeClass('ng-hide');
                         }, 500);
                     }
                 } else {
+                    $('.daiquiri-query-submitting').addClass('ng-hide');
                     $timeout.cancel(scope.timeout);
-                    element.addClass('ng-hide');
 
                     scope.timeout = null;
                 }
