52ab0d52ee9317cdbf5d6883b3cf7a930d995f5f
Jochen Klar <jklar@aip.de>
2018-04-23 12:46:53 +0200

    Fix tap table serializer

--- daiquiri/tap/serializers.py
+++ daiquiri/tap/serializers.py
@@ -1,6 +1,7 @@
+from django.conf import settings
+
 from rest_framework import serializers
 
-from daiquiri.query.models import Example
 from daiquiri.metadata.models import Schema, Table, Column
 
 
@@ -37,7 +38,11 @@ class TableSerializer(serializers.ModelSerializer):
 
     def get_columns(self, obj):
         # filter the columns which are published for the groups of the user
-        queryset = obj.columns.filter_by_access_level(self.context['request'].user)
+        if settings.METADATA_COLUMN_PERMISSIONS:
+            queryset = obj.columns.filter_by_access_level(self.context['request'].user)
+        else:
+            queryset = obj.columns.all()
+
         return ColumnSerializer(queryset, context=self.context, many=True).data
 
 
