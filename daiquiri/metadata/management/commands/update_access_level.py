b94f398732db4fd476e9db704047753a197162d6
Jochen Klar <jklar@aip.de>
2018-04-24 14:28:38 +0200

    Add update_access_level management script

---
+++ daiquiri/metadata/management/commands/update_access_level.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand,CommandError
+from django.utils.translation import ugettext_lazy as _
+
+from daiquiri.core.constants import ACCESS_LEVEL_CHOICES
+from daiquiri.metadata.models import Schema
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('schema', help='the schema to be updated')
+        parser.add_argument('access_level', help='new access_level and metadata_access_level')
+
+    def handle(self, *args, **options):
+
+        if options['access_level'] not in dict(ACCESS_LEVEL_CHOICES):
+            raise CommandError(_('Unknown access_level.'))
+
+        schema = Schema.objects.get(name=options['schema'])
+        schema.access_level = options['access_level']
+        schema.metadata_access_level = options['access_level']
+        schema.save()
+
+        for table in schema.tables.all():
+            table.access_level = options['access_level']
+            table.metadata_access_level = options['access_level']
+            table.save()
