e961c2735df740bdc88b7c766f04ffee1907be63
Jochen Klar <jklar@aip.de>
2018-04-25 10:06:44 +0200

    Release 0.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'typing',
         'mock',
         'coverage',
-        'queryparser_python%d>=0.2.6' % sys.version_info.major
+        'queryparser_python%d>=0.3' % sys.version_info.major
     ],
     classifiers=[
         'Development Status :: 1 - Planning',
