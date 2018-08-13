80ad14a7c33b92759e3d823c6796227bfb49f050
Jochen Klar <jklar@aip.de>
2018-08-13 11:54:52 +0200

    Add LOGOUT_REDIRECT_URL to settings

--- daiquiri/core/settings/base.py
+++ daiquiri/core/settings/base.py
@@ -104,6 +104,7 @@ USE_TZ = True
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_URL = '/accounts/logout/'
+LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
