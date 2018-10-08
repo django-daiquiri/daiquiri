ecf8a20028b05577585682aa35fd1ac99dc20dcf
Jochen Klar <jklar@aip.de>
2018-10-08 15:51:40 +0200

    Add logging to files:file

--- daiquiri/files/viewsets.py
+++ daiquiri/files/viewsets.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import viewsets
@@ -6,6 +8,8 @@ from rest_framework.response import Response
 
 from .utils import check_file, search_file, send_file
 
+logger = logging.getLogger(__name__)
+
 
 class FileViewSet(viewsets.GenericViewSet):
 
@@ -13,10 +17,16 @@ class FileViewSet(viewsets.GenericViewSet):
 
         search = request.GET.get('search', None)
 
+        logger.debug('search = "%s"', search)
+
         if search:
             file_path = search_file(search)
+            has_permission = check_file(request.user, file_path)
 
-            if file_path and check_file(request.user, file_path):
+            logger.debug('file_path = "%s"', file_path)
+            logger.debug('result = "%s"', has_permission)
+
+            if file_path and has_permission:
 
                 if request.GET.get('download', True):
                     return send_file(request, file_path, search)
