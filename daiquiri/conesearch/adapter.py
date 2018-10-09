757be939bb3f569969e77da11c15a74a41626081
Jochen Klar <jklar@aip.de>
2018-10-09 16:57:50 +0200

    Add TableConeSearchAdapter

--- daiquiri/conesearch/adapter.py
+++ daiquiri/conesearch/adapter.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.http import FileResponse
+from django.utils.translation import ugettext_lazy as _
 
-from rest_framework.exceptions import NotFound, ValidationError
+from rest_framework.exceptions import ValidationError, NotFound
 
 from daiquiri.core.adapter import DatabaseAdapter
 from daiquiri.core.adapter.stream import BaseServiceAdapter
-from daiquiri.core.utils import import_class
+from daiquiri.core.utils import import_class, make_query_dict_upper_case
 from daiquiri.core.generators import generate_votable
 
 
@@ -81,6 +82,74 @@ AND
         }
 
         self.args, errors = self.parse_query_dict(request)
+
+        if errors:
+            raise ValidationError(errors)
+
+
+class TableConeSearchAdapter(BaseConeSearchAdapter):
+
+    def clean(self, request, resource):
+        from daiquiri.metadata.models import Schema, Table
+
+        if resource not in self.resources:
+            raise NotFound
+
+        schema_name = self.resources[resource]['schema_name']
+        table_name = self.resources[resource]['table_name']
+
+        data = make_query_dict_upper_case(request.GET)
+        errors = {}
+
+        # check if the user is allowed to access the schema
+        try:
+            schema = Schema.objects.filter_by_access_level(request.user).get(name=schema_name)
+        except Schema.DoesNotExist:
+            raise NotFound
+
+        # check if the user is allowed to access the table
+        try:
+            table = Table.objects.filter_by_access_level(request.user).filter(schema=schema).get(name=table_name)
+        except Table.DoesNotExist:
+            raise NotFound
+
+        # fetch the columns accoring to the verbosity
+        verb = data.get('VERB', '2')
+
+        if verb == '1':
+            self.columns = table.columns.filter(name__in=self.resources[resource]['column_names']).values()
+        elif verb == '2':
+            self.columns = table.columns.filter(principal=True).values()
+        elif verb == '3':
+            self.columns = table.columns.values()
+        else:
+            errors['VERB'] = [_('This field must be 1, 2, or 3.')]
+
+        # construct sql query
+        adapter = DatabaseAdapter()
+        escaped_column_names = [adapter.escape_identifier(column['name']) for column in self.columns]
+        self.sql = self.sql_pattern % {
+            'schema': adapter.escape_identifier(schema_name),
+            'table': adapter.escape_identifier(table_name),
+            'columns': ', '.join(escaped_column_names)
+        }
+
+        # parse RA, DEC, and SR arguments
+        self.args = {}
+        for key in ['RA', 'DEC', 'SR']:
+            try:
+                value = float(data[key])
+
+                if self.ranges[key]['min'] <= value <= self.ranges[key]['max']:
+                    self.args[key] = value
+                else:
+                    errors[key] = [_('This value must be between %(min)g and %(max)g.' % self.ranges[key])]
+
+            except KeyError:
+                errors[key] = [_('This field may not be blank.')]
+
+            except ValueError:
+                errors[key] = [_('This field must be a float.')]
 
         if errors:
             raise ValidationError(errors)
