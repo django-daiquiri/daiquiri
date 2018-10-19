5cfb504ea0a25692ba404f15ee306905ac36d78e
Jochen Klar <jklar@aip.de>
2018-10-19 17:29:15 +0200

    Remove static from STATICFILES_FINDERS

--- daiquiri/core/settings/base.py
+++ daiquiri/core/settings/base.py
@@ -114,7 +114,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'vendor/'),
-    os.path.join(BASE_DIR, 'static/'),
 )
 
 STATICFILES_FINDERS = (
