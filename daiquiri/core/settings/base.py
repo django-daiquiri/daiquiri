bfae66a48c47b094da37597d9f9b1ee00a9d742d
Jochen Klar <jochenklar@gmail.com>
2018-12-10 14:15:18 +0100

    Revert "Add static/ to STATICFILES_DIRS"
    This reverts commit 74c5437c1d96d250826a700e723fd7c358ac32ea.

--- daiquiri/core/settings/base.py
+++ daiquiri/core/settings/base.py
@@ -114,7 +114,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
     os.path.join(BASE_DIR, 'vendor/'),
 )
 
