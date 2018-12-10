74c5437c1d96d250826a700e723fd7c358ac32ea
Jochen Klar <jochenklar@gmail.com>
2018-12-10 09:50:58 +0100

    Add static/ to STATICFILES_DIRS

--- daiquiri/core/settings/base.py
+++ daiquiri/core/settings/base.py
@@ -114,6 +114,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
     os.path.join(BASE_DIR, 'vendor/'),
 )
 
