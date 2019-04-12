9bab83deb6e8daa97d3e2f844553f467857c1b97
Jochen Klar <mail@jochenklar.de>
2019-04-12 12:17:30 +0200

    Update for Django 2.2

--- daiquiri/core/views.py
+++ daiquiri/core/views.py
@@ -34,7 +34,7 @@ def not_found(request, exception):
     return render(request, 'core/404.html', status=404)
 
 
-def internal_server_error(request, exception):
+def internal_server_error(request):
     return render(request, 'core/500.html', status=500)
 
 
