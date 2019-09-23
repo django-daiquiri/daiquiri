dfa44ad6134ca6a0f204c68bdc1333ddb52318ad
Jochen Klar <mail@jochenklar.de>
2019-09-23 11:46:54 +0200

    Fix settings

--- daiquiri/core/settings/celery.py
+++ daiquiri/core/settings/celery.py
@@ -2,7 +2,7 @@ from kombu import Exchange, Queue
 
 import daiquiri.core.env as env
 
-CELERY_BROKER_URL = env.get(CELERY_BROKER_URL, 'amqp://')
+CELERY_BROKER_URL = env.get('CELERY_BROKER_URL', 'amqp://')
 
 CELERY_TASK_ACKS_LATE = True
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1
