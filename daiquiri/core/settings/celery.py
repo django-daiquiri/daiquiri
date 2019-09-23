32b4a953a1f59e29a71c06271916227917a04cbc
Jochen Klar <mail@jochenklar.de>
2019-09-23 11:43:16 +0200

    Get CELERY_BROKER_URL from env

--- daiquiri/core/settings/celery.py
+++ daiquiri/core/settings/celery.py
@@ -1,6 +1,8 @@
 from kombu import Exchange, Queue
 
-CELERY_BROKER_URL = 'amqp://'
+import daiquiri.core.env as env
+
+CELERY_BROKER_URL = env.get(CELERY_BROKER_URL, 'amqp://')
 
 CELERY_TASK_ACKS_LATE = True
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1
