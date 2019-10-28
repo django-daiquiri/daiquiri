968a192c9b9530139e803c512eab4d84ef4cd4ff
Jochen Klar <mail@jochenklar.de>
2019-10-28 13:32:27 +0100

    Add activate_user script

---
+++ daiquiri/core/management/commands/activate_user.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', help='Username of the user to activate.')
+        parser.add_argument('--deactivate', action='store_true', default=False, help='Deactivate instead of activate.')
+
+    def handle(self, *args, **options):
+        is_active = False if options['activate'] else True
+
+        user = User.objects.get(username=options['username'])
+        user.is_active = is_active
+        user.save()
