7838a96dc0d6bab7b29db4abf8db8dcf14ad06e1
Jochen Klar <mail@jochenklar.de>
2019-10-28 13:33:41 +0100

    Fix activate_user script

--- daiquiri/core/management/commands/activate_user.py
+++ daiquiri/core/management/commands/activate_user.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         parser.add_argument('--deactivate', action='store_true', default=False, help='Deactivate instead of activate.')
 
     def handle(self, *args, **options):
-        is_active = False if options['activate'] else True
+        is_active = False if options['deactivate'] else True
 
         user = User.objects.get(username=options['username'])
         user.is_active = is_active
