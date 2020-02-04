f3c4cf6687722cb41db20d7d7f70aacc9cbd86e4
Jochen Klar <jochenklar@gmail.com>
2020-02-04 12:14:05 +0100

    Improve Profile in django admin

--- daiquiri/auth/admin.py
+++ daiquiri/auth/admin.py
@@ -6,6 +6,7 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email')
     list_display = ('__str__', 'is_pending', 'is_confirmed')
+    list_filter = ('is_pending', 'is_confirmed', 'user__is_staff', 'user__is_superuser')
     readonly_fields = ('user', 'consent')
 
 
