154a1888535f9da29e5691e4c65eede138e83663
Jochen Klar <jochenklar@gmail.com>
2020-02-04 12:11:33 +0100

    Fix Profile search in django admin

--- daiquiri/auth/admin.py
+++ daiquiri/auth/admin.py
@@ -4,8 +4,9 @@ from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ('__str__', )
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email')
     list_display = ('__str__', 'is_pending', 'is_confirmed')
     readonly_fields = ('user', 'consent')
+
 
 admin.site.register(Profile, ProfileAdmin)
