9c194b398c291df30896d481a3bc10d5f787ff4a
Jochen Klar <mail@jochenklar.de>
2022-01-10 16:53:26 +0100

    Disable handlers for fixture load

--- daiquiri/metadata/handlers.py
+++ daiquiri/metadata/handlers.py
@@ -8,32 +8,33 @@ from .models import Schema, Table, Column
 
 @receiver(post_save, sender=Schema)
 def schema_updated_handler(sender, **kwargs):
-    if apps.is_installed('daiquiri.oai'):
+    if not kwargs.get('raw') and apps.is_installed('daiquiri.oai'):
         from daiquiri.oai.utils import update_records
         update_records('schema', kwargs['instance'])
 
 
 @receiver(post_delete, sender=Schema)
 def schema_deleted_handler(sender, **kwargs):
-    if apps.is_installed('daiquiri.oai'):
+    if not kwargs.get('raw') and apps.is_installed('daiquiri.oai'):
         from daiquiri.oai.utils import delete_records
         delete_records('schema', kwargs['instance'])
 
 
 @receiver(post_save, sender=Table)
 def table_updated_handler(sender, **kwargs):
-    if apps.is_installed('daiquiri.oai'):
+    if not kwargs.get('raw') and apps.is_installed('daiquiri.oai'):
         from daiquiri.oai.utils import update_records
         update_records('table', kwargs['instance'])
 
 
 @receiver(post_delete, sender=Table)
 def table_deleted_handler(sender, **kwargs):
-    if apps.is_installed('daiquiri.oai'):
+    if not kwargs.get('raw') and apps.is_installed('daiquiri.oai'):
         from daiquiri.oai.utils import delete_records
         delete_records('table', kwargs['instance'])
 
 
 @receiver(post_save, sender=Column)
 def column_updated_handler(sender, **kwargs):
-    cache.delete('processor')
+    if not kwargs.get('raw'):
+        cache.delete('processor')
--- daiquiri/tap/handlers.py
+++ daiquiri/tap/handlers.py
@@ -15,29 +15,35 @@ from .utils import (
 
 @receiver(post_save, sender=Schema)
 def schema_updated_handler(sender, **kwargs):
-    update_schema(kwargs['instance'])
+    if not kwargs.get('raw'):
+        update_schema(kwargs['instance'])
 
 
 @receiver(post_delete, sender=Schema)
 def schema_deleted_handler(sender, **kwargs):
-    delete_schema(kwargs['instance'])
+    if not kwargs.get('raw'):
+        delete_schema(kwargs['instance'])
 
 
 @receiver(post_save, sender=Table)
 def table_updated_handler(sender, **kwargs):
-    update_table(kwargs['instance'])
+    if not kwargs.get('raw'):
+        update_table(kwargs['instance'])
 
 
 @receiver(post_delete, sender=Table)
 def table_deleted_handler(sender, **kwargs):
-    delete_table(kwargs['instance'])
+    if not kwargs.get('raw'):
+        delete_table(kwargs['instance'])
 
 
 @receiver(post_save, sender=Column)
 def column_updated_handler(sender, **kwargs):
-    update_column(kwargs['instance'])
+    if not kwargs.get('raw'):
+        update_column(kwargs['instance'])
 
 
 @receiver(post_delete, sender=Column)
 def column_deleted_handler(sender, **kwargs):
-    delete_column(kwargs['instance'])
+    if not kwargs.get('raw'):
+        delete_column(kwargs['instance'])
