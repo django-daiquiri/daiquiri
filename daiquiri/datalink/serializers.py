81e20d60369483615311df2d094f84aa2a235be8
Jochen Klar <mail@jochenklar.de>
2022-01-17 17:17:53 +0100

    Add parameters to datalink

--- daiquiri/datalink/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from daiquiri.jobs.serializers import SyncJobSerializer
-
-
-class SyncDatalinkJobSerializer(SyncJobSerializer):
-
-    RESPONSEFORMAT = serializers.CharField(required=False)
-    FORMAT = serializers.CharField(required=False)
--- daiquiri/datalink/viewsets.py
+++ daiquiri/datalink/viewsets.py
@@ -4,19 +4,30 @@ from daiquiri.core.generators import generate_votable
 from daiquiri.jobs.viewsets import SyncJobViewSet
 
 from .constants import DATALINK_FIELDS, DATALINK_CONTENT_TYPE
-from .serializers import SyncDatalinkJobSerializer
+
 from .models import Datalink
 
 
 class SyncDatalinkJobViewSet(SyncJobViewSet):
-    serializer_class = SyncDatalinkJobSerializer
 
-    parameter_map = {
-        'ID': 'id',
-        'RESPONSEFORMAT': 'response_format'
-    }
+    def list(self, request):
+        return self.perform_sync_job(request, request.GET)
+
+    def create(self, request):
+        return self.perform_sync_job(request, request.POST)
 
     def perform_sync_job(self, request, data):
-        rows = Datalink.objects.values_list()
+        rows = []
+
+        if 'ID' in data:
+            identifiers = data.getlist('ID')
+            field_names = [field['name'] for field in DATALINK_FIELDS]
+            rows = list(Datalink.objects.filter(ID__in=identifiers).values_list(*field_names))
+
+            # check for missing IDs
+            for identifier in identifiers:
+                if not any(filter(lambda row: row[0] == identifier, rows)):
+                    rows.append((identifier, None, None, 'Not found: {}'.format(identifier), None, None, None, None))
+
         datalink_table = generate_votable(rows, DATALINK_FIELDS)
         return FileResponse(datalink_table, content_type=DATALINK_CONTENT_TYPE)
