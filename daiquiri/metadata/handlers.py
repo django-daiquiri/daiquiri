b928e6cf8ecb3d5d37fdc1ec30a68f950a0cc666
Jochen Klar <mail@jochenklar.de>
2022-01-24 15:02:11 +0100

    Move tap handlers to metadata app

--- daiquiri/metadata/handlers.py
+++ daiquiri/metadata/handlers.py
@@ -17,6 +17,10 @@ def schema_updated_handler(sender, **kwargs):
             from daiquiri.datalink.utils import update_links
             update_links('schema', kwargs['instance'])
 
+        if apps.is_installed('daiquiri.tap'):
+            from daiquiri.tap.utils import update_schema
+            update_schema(kwargs['instance'])
+
 
 @receiver(post_delete, sender=Schema)
 def schema_deleted_handler(sender, **kwargs):
@@ -29,31 +33,63 @@ def schema_deleted_handler(sender, **kwargs):
             from daiquiri.datalink.utils import delete_links
             delete_links('schema', kwargs['instance'])
 
+        if apps.is_installed('daiquiri.tap'):
+            from daiquiri.tap.utils import delete_schema
+            delete_schema(kwargs['instance'])
+
 
 @receiver(post_save, sender=Table)
 def table_updated_handler(sender, **kwargs):
+    table = kwargs['instance']
+
     if not kwargs.get('raw'):
         if apps.is_installed('daiquiri.oai'):
             from daiquiri.oai.utils import update_records
-            update_records('table', kwargs['instance'])
+            update_records('table', table)
 
         if apps.is_installed('daiquiri.datalink'):
             from daiquiri.datalink.utils import update_links
-            update_links('table', kwargs['instance'])
+            update_links('table', table)
+
+        if apps.is_installed('daiquiri.tap'):
+            from daiquiri.tap.utils import update_table
+            update_table(table)
 
 
 @receiver(post_delete, sender=Table)
 def table_deleted_handler(sender, **kwargs):
+    table = kwargs['instance']
+
     if not kwargs.get('raw'):
         if apps.is_installed('daiquiri.oai'):
             from daiquiri.oai.utils import delete_records
-            delete_records('table', kwargs['instance'])
+            delete_records('table', table)
 
         if apps.is_installed('daiquiri.datalink'):
             from daiquiri.datalink.utils import delete_links
-            delete_links('table', kwargs['instance'])
+            delete_links('table', table)
+
+        if apps.is_installed('daiquiri.tap'):
+            from daiquiri.datalink.utils import delete_table
+            delete_table(table)
 
 
 @receiver(post_save, sender=Column)
 def column_updated_handler(sender, **kwargs):
+    column = kwargs['instance']
+
+    # clear cached queryparser
     cache.delete('processor')
+
+    if not kwargs.get('raw'):
+        from daiquiri.datalink.utils import update_column
+        update_column(column)
+
+
+@receiver(post_delete, sender=Column)
+def column_deleted_handler(sender, **kwargs):
+    column = kwargs['instance']
+
+    if not kwargs.get('raw'):
+        from daiquiri.datalink.utils import delete_column
+        delete_column(column)
