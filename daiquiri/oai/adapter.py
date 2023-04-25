33d74b61c128a78f66eaf821087c9b27c539f764
Kirill Makan <kmakan@aip.de>
2023-04-25 18:45:37 +0200

    fix the repetition of the formats generated from datalink

--- daiquiri/oai/adapter.py
+++ daiquiri/oai/adapter.py
@@ -246,7 +246,8 @@ class DatalinkOAIAdapterMixin(object):
                 datalink['title'] = description
 
             elif semantics == '#this':
-                datalink['formats'].append(content_type)
+                if content_type not in datalink['formats']:
+                    datalink['formats'].append(content_type)
                 datalink['related_identifiers'].append({
                     'related_identifier': access_url,
                     'related_identifier_type': 'URL',
@@ -254,7 +255,8 @@ class DatalinkOAIAdapterMixin(object):
                 })
 
             elif semantics == '#detached-header':
-                datalink['formats'].append(content_type)
+                if content_type not in datalink['formats']:
+                    datalink['formats'].append(content_type)
                 datalink['related_identifiers'].append({
                     'related_identifier': access_url,
                     'related_identifier_type': 'URL',
