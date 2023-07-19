8346994b4e3dad81b129f5a6503f62b22be7117f
Kirill Makan <kmakan@aip.de>
2023-07-19 09:56:26 +0200

    update metadata for release

--- daiquiri/__init__.py
+++ daiquiri/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'django-daiquiri'
-__version__ = '0.4.0'
+__version__ = '0.4.2'
 __author__ = 'Jochen Klar'
 __email__ = 'mail@jochenklar.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2016-2022 Leibniz Institute for Astrophysics Potsdam (AIP)'
+__copyright__ = 'Copyright 2016-2023 Leibniz Institute for Astrophysics Potsdam (AIP)'
 
 VERSION = __version__
