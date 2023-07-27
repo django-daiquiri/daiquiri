23d9c903a4492e3e3027705e290a890a6f4c0e73
fournier <y.fournier@aip.de>
2023-07-27 16:32:33 +0200

    added a validator for the TableSerialiser and SchemaSerialiser of metadata for validation the content of the JSON fields for creators and contributors

---
+++ daiquiri/metadata/serializers/validators.py
@@ -0,0 +1,31 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.exceptions import ValidationError
+
+class PersonListValidator(object):
+
+    def __call__(self, persons):
+
+        for person in persons:
+
+            if not isinstance(person, dict):
+                raise ValidationError("the person field must be a JSON Field")
+      
+            affiliations = person.get("affiliations", [])
+
+            if isinstance(affiliations, list):
+                for affiliation in affiliations:
+                
+                    if isinstance(affiliation, dict):
+                        if affiliation != {}: # if not empty dict should have at least affiliation name
+                            affiliation_name = affiliation.get("affiliation", None)
+
+                            if affiliation_name is None:
+                                raise ValidationError('affiliation needs at least a name: "affiliation": <name>')
+                        else:
+                            raise ValidationError("empty affiliations are not valid")
+                    else:
+                        raise ValidationError("each affiliation must be a JSON field")
+
+            else:
+                raise ValidationError("affiliations must be a list of JSON Fields")
