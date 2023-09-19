b4fd9d3b6b8ca6b8cfc39112d1a4dbb207ca9f47
kimakan <45099849+kimakan@users.noreply.github.com>
2023-09-19 11:17:29 +0200

    add visibility conditions to the announcement messages and refactor

--- daiquiri/contact/admin.py
+++ daiquiri/contact/admin.py
@@ -1,11 +1,17 @@
+from django import forms
 from django.contrib import admin
 
-from .models import ContactMessage, AnnouncementMessage
+from .models import ContactMessage, AnnouncementMessage, MessageFilter
+
+
+class AnnouncementMessageAdminForm(forms.ModelForm):
+    visibility_filter = forms.ChoiceField(choices=MessageFilter().CHOICES)
 
 
 @admin.action(description="Make selected messages visible")
 def make_visible(modeladmin, request, queryset):
     queryset.update(visible=True)
+
 
 @admin.action(description="Make selected messages invisible")
 def make_invisible(modeladmin, request, queryset):
@@ -16,7 +22,10 @@ class ContactMessageAdmin(admin.ModelAdmin):
     search_fields = ("subject", "email", "author", "status", "user__username")
     list_display = ("subject", "email", "author", "status")
 
+
 class AnnouncementMessageAdmin(admin.ModelAdmin):
+    form = AnnouncementMessageAdminForm
+
     search_fields = ("title", "announcement")
     list_display = ("title", "visible", "announcement", "announcement_type", "updated")
     actions = [make_visible, make_invisible]
--- daiquiri/contact/templatetags/announcement_tags.py
+++ daiquiri/contact/templatetags/announcement_tags.py
@@ -4,9 +4,11 @@ from daiquiri.contact.models import AnnouncementMessage
 register = template.Library()
 
 
-@register.inclusion_tag("contact/announcements.html")
-def show_announcements():
+@register.inclusion_tag("contact/announcements.html", takes_context=True)
+def show_announcements(context):
+    request = context["request"]
     announcements = AnnouncementMessage.objects.filter(visible=True)
+    announcements = [msg for msg in announcements if msg.get_filter()(request) is True]
     return {
         "announcements": announcements,
     }
