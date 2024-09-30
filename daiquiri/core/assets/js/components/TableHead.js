b46f335689e732f96ab90741e5e3a2d789dacb10
Jochen Klar <mail@jochenklar.de>
2024-09-30 17:28:35 +0200

    Add resizing to table

--- daiquiri/core/assets/js/components/TableHead.js
+++ daiquiri/core/assets/js/components/TableHead.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+
+import TableHandle from './TableHandle'
 
 const TableHead = ({ columns, params, setParams }) => {
   const tooltips = true
   const ordering = params.ordering || ''
+
+  const [widths, setWidths] = useState(columns.map(() => 300))
 
   const handleOrdering = (column) => {
     setParams({...params, ordering: (ordering == column.name) ? '-' + column.name : column.name})
@@ -15,8 +19,13 @@ const TableHead = ({ columns, params, setParams }) => {
       <tr>
         {
           columns.map((column, columnIndex) => (
-            <th key={columnIndex}>
+            <th key={columnIndex} style={{ width: widths[columnIndex] }}>
               <div className="dq-table-cell">
+                {
+                  columnIndex > 0 && (
+                    <TableHandle columnIndex={columnIndex - 1} widths={widths} setWidths={setWidths} />
+                  )
+                }
                 <div className="name">
                   {
                     column.label ? (
@@ -48,8 +57,8 @@ const TableHead = ({ columns, params, setParams }) => {
                   }
                 </div>
                 {
-                  columnIndex < columns.length -1 && (
-                    <div className="handle"></div>
+                  columnIndex < (columns.length - 1) && (
+                    <TableHandle columnIndex={columnIndex} widths={widths} setWidths={setWidths} />
                   )
                 }
               </div>
