bf5f7d867405856f92de9924eda2257861ec2c79
simeonreusch <mail@simeonreusch.com>
2025-03-10 13:58:58 +0100

    include always generated file in dev branch, autoformat, update .gitignore to ignore core dumps

--- daiquiri/files/utils.py
+++ daiquiri/files/utils.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 from django.utils.encoding import force_str
 from django.utils.safestring import mark_safe
 from django.utils.timezone import now
-
 from django_sendfile import sendfile
 
 from daiquiri.core.utils import get_client_ip, markdown
@@ -25,12 +24,14 @@ def file_exists(file_path):
 def get_file_path(file_path):
     if file_exists(file_path):
         return file_path
-    elif not file_path or file_path.endswith('/'):
+    elif not file_path or file_path.endswith("/"):
         # try different paths
-        for path in [file_path.rstrip('/') + '.html',
-                     file_path.rstrip('/') + '.md',
-                     file_path + 'index.html',
-                     file_path + 'index.md']:
+        for path in [
+            file_path.rstrip("/") + ".html",
+            file_path.rstrip("/") + ".md",
+            file_path + "index.html",
+            file_path + "index.md",
+        ]:
             if file_exists(path):
                 return path
 
@@ -40,10 +41,12 @@ def get_file_path(file_path):
 
 def get_directory(user, file_path):
     # loop over all directories beginning with the highest depth and return as soon as a directory matches
-    for directory in Directory.objects.order_by('-depth'):
+    for directory in Directory.objects.order_by("-depth"):
         if os.path.normpath(file_path).startswith(directory.path):
             try:
-                return Directory.objects.filter_by_access_level(user).get(pk=directory.pk)
+                return Directory.objects.filter_by_access_level(user).get(
+                    pk=directory.pk
+                )
             except Directory.DoesNotExist:
                 return None
 
@@ -53,27 +56,24 @@ def check_file(user, file_path):
 
 
 def render_with_layout(request, file_path):
-
     context = {}
     absolute_file_path = os.path.join(settings.FILES_BASE_PATH, file_path)
     content = read_file_content(absolute_file_path)
     if content:
         context["content"] = content
 
-    return render(request, 'files/layout.html', context)
-
+    return render(request, "files/layout.html", context)
 
 
 def read_file_content(abs_file_path):
-    """ Reads the content of a html- or md-file and returns html
-    """
-    if abs_file_path.endswith('.html') or abs_file_path.endswith('.md'):
+    """Reads the content of a html- or md-file and returns html"""
+    if abs_file_path.endswith(".html") or abs_file_path.endswith(".md"):
         with open(abs_file_path) as f:
             file_content = f.read()
 
-            if abs_file_path.endswith('.html'):
+            if abs_file_path.endswith(".html"):
                 return mark_safe(file_content)
-            elif abs_file_path.endswith('.md'):
+            elif abs_file_path.endswith(".md"):
                 return mark_safe(force_str(markdown(file_content)))
     else:
         return ""
@@ -81,21 +81,19 @@ def read_file_content(abs_file_path):
 
 def send_file(request, file_path, search=None):
     # create a stats record for this download
-    resource = {
-        'file_path': file_path
-    }
+    resource = {"file_path": file_path}
     if search:
-        resource['search'] = search
+        resource["search"] = search
 
     absolute_file_path = os.path.join(settings.FILES_BASE_PATH, file_path)
 
     Record.objects.create(
         time=now(),
-        resource_type='FILE',
+        resource_type="FILE",
         resource=resource,
         client_ip=get_client_ip(request),
         user=request.user if request.user.is_authenticated else None,
-        size=os.path.getsize(absolute_file_path)
+        size=os.path.getsize(absolute_file_path),
     )
 
     # send the file to the client
