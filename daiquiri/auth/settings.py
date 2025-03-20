2e3f677030139018ca7bc02dfde277a3157a3c0f
simeonreusch <mail@simeonreusch.com>
2025-03-20 15:38:51 +0100

    move more sensible defaults to daiquiri

--- daiquiri/auth/settings.py
+++ daiquiri/auth/settings.py
@@ -1,7 +1,8 @@
 import daiquiri.core.env as env
 
-AUTH_SIGNUP = env.get_bool('AUTH_SIGNUP')
+# AUTH_SIGNUP = env.get_bool('AUTH_SIGNUP')
+AUTH_SIGNUP = True
 
 AUTH_WORKFLOW = None
 AUTH_DETAIL_KEYS = []
-AUTH_TERMS_OF_USE = False
+AUTH_TERMS_OF_USE = True
