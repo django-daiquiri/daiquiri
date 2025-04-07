1b6681d5f989854fc8e5f50ee75016cc4db22619
kimakan <45099849+kimakan@users.noreply.github.com>
2025-04-07 16:59:26 +0200

    Fix auth settings

--- daiquiri/auth/settings.py
+++ daiquiri/auth/settings.py
@@ -1,7 +1,6 @@
 import daiquiri.core.env as env
 
-# AUTH_SIGNUP = env.get_bool('AUTH_SIGNUP')
-AUTH_SIGNUP = True
+AUTH_SIGNUP = env.get_bool('AUTH_SIGNUP')
 
 AUTH_WORKFLOW = None
 AUTH_DETAIL_KEYS = []
