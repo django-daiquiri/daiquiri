d55c215ab01d74181f18544d675b1f6156467450
simeonreusch <mail@simeonreusch.com>
2025-04-07 13:10:08 +0200

    add ref.id to parsed ucd meta entries

--- daiquiri/core/assets/js/utils/table.js
+++ daiquiri/core/assets/js/utils/table.js
@@ -23,6 +23,8 @@ export const isNoteColumn = (column) => column.ucd && column.ucd.includes('meta.
 
 export const isFileColumn = (column) => column.ucd && column.ucd.includes('meta.file')
 
+export const isRefIDColumn = (column) => column.ucd && column.ucd.includes('meta.ref.id')
+
 export const isModalColumn = (column) => isRefColumn(column) && (
     isDataLinkColumn(column) || isImageColumn(column) || isNoteColumn(column)
-)
+) && !isRefIDColumn(column)
