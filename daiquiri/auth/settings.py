f74342ab144b1c8e60a029ffb85dc7023a797c3e
simeonreusch <mail@simeonreusch.com>
2025-05-19 16:00:20 +0200

    Revert auth settings to main

--- daiquiri/auth/settings.py
+++ daiquiri/auth/settings.py
@@ -1,8 +1,7 @@
 import daiquiri.core.env as env
 
-# AUTH_SIGNUP = env.get_bool('AUTH_SIGNUP')
-AUTH_SIGNUP = True
+AUTH_SIGNUP = env.get_bool('AUTH_SIGNUP')
 
 AUTH_WORKFLOW = None
 AUTH_DETAIL_KEYS = []
-AUTH_TERMS_OF_USE = True
+AUTH_TERMS_OF_USE = False
