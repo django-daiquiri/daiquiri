3a1244db36a8b5609c7815aec75f924730fd1035
simeonreusch <mail@simeonreusch.com>
2025-08-21 16:09:11 +0200

    replace deprecated crypt module with passlib, keeping with the existing SHA512 setup for Unix hash compatibility

--- daiquiri/core/hashers.py
+++ daiquiri/core/hashers.py
@@ -1,39 +1,66 @@
-# inspired by https://djangosnippets.org/snippets/10572/
-
 from collections import OrderedDict
 
-from django.contrib.auth.hashers import CryptPasswordHasher, mask_hash
-from django.utils.crypto import constant_time_compare, get_random_string
+from django.contrib.auth.hashers import BasePasswordHasher, mask_hash
+from django.utils.crypto import get_random_string
 from django.utils.encoding import force_str
 from django.utils.translation import gettext_noop as _
 
+from passlib.hash import sha512_crypt
 
-class CrypdSHA512PasswordHasher(CryptPasswordHasher):
+
+class CrypdSHA512PasswordHasher(BasePasswordHasher):
+    """
+    SHA-512-crypt password hasher using passlib.
+    Compatible with Unix /etc/shadow ($6$salt$hash).
+    """
 
     algorithm = 'crypt_sha512'
 
     def salt(self):
-        return '$6$' + get_random_string(8)
+        return get_random_string(8)
 
     def encode(self, password, salt):
-        crypt = self._load_library()
-        data = crypt.crypt(force_str(password), salt)
-        return f"{self.algorithm}{data}"
+        """
+        Encode a password using SHA-512-crypt via passlib.
+        Returns format: crypt_sha512$6$salt$hash
+        """
+        password = force_str(password)
+        stored = sha512_crypt.using(rounds=5000, salt=salt).hash(password)
+        return f'{self.algorithm}${stored.lstrip("$")}'
 
     def verify(self, password, encoded):
-        crypt = self._load_library()
-        algorithm, rest = encoded.split('$', 1)
-        salt, hash = rest.rsplit('$', 1)
-        salt = '$' + salt
-        assert algorithm == self.algorithm
-        return constant_time_compare(f'{salt}${hash}', crypt.crypt(force_str(password), salt))
+        """
+        Verify a password against a stored hash.
+        `encoded` format: crypt_sha512$6$salt$hash
+        """
+        try:
+            algorithm, rest = encoded.split('$', 1)
+            if algorithm != self.algorithm:
+                return False
+            stored = '$' + rest  # reconstruct $6$salt$hash for passlib
+            return sha512_crypt.verify(force_str(password), stored)
+        except Exception:
+            return False
 
     def safe_summary(self, encoded):
-        algorithm, prefix, salt, hash = encoded.split('$')
-        assert algorithm == self.algorithm
-        return OrderedDict([
-            (_('algorithm'), algorithm),
-            (_('prefix'), prefix),
-            (_('salt'), mask_hash(salt)),
-            (_('hash'), mask_hash(hash)),
-        ])
+        """
+        Provide a masked summary for admin display.
+        """
+        try:
+            algorithm, prefix, salt, hash = encoded.split('$')
+            assert algorithm == self.algorithm
+            return OrderedDict(
+                [
+                    (_('algorithm'), algorithm),
+                    (_('prefix'), prefix),
+                    (_('salt'), mask_hash(salt)),
+                    (_('hash'), mask_hash(hash)),
+                ]
+            )
+        except ValueError:
+            return OrderedDict(
+                [
+                    (_('algorithm'), self.algorithm),
+                    (_('hash'), mask_hash(encoded)),
+                ]
+            )
