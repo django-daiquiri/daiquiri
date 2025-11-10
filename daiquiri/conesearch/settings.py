8498ab88d02192cf7eecd993e09aa7973900c8fa
Wail Ahmed <wahmed@canopus.aip.de>
2025-11-10 16:59:59 +0100

    add the clean method to BaseConeSearchAdapter and remove unused classes

--- daiquiri/conesearch/settings.py
+++ daiquiri/conesearch/settings.py
@@ -3,9 +3,11 @@ import daiquiri.core.env as env
 CONESEARCH_ADAPTER = 'daiquiri.conesearch.adapter.SimpleConeSearchAdapter'
 CONESEARCH_ANONYMOUS = False
 
-CONESEARCH_SCHEMA = env.get('CONESEARCH_SCHEMA')
-CONESEARCH_TABLE = env.get('CONESEARCH_TABLE')
+#CONESEARCH_SCHEMA = env.get('CONESEARCH_SCHEMA')
+#CONESEARCH_TABLE = env.get('CONESEARCH_TABLE')
 
 CONESEARCH_SUBJECTS = [
     'cone search'
 ]
+
+CONESEARCH_RESOURCES = {}
