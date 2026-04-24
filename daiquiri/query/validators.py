5829f8527c22b4e6297b3e1d97f0c8f34d86abf9
wail-abbas <wailabbas.ahmed@gmail.com>
2026-04-24 14:19:24 +0200

    generalize query/validators

--- daiquiri/query/validators.py
+++ daiquiri/query/validators.py
@@ -23,15 +23,11 @@ class TableNameValidator:
     def __call__(self, table_name, serializer_field):
         request = serializer_field.parent.context['request']
         user = None if request.user.is_anonymous else request.user
-        instance = serializer_field.parent.instance
-        current_table_name = getattr(instance, 'table_name', None) if instance else None
-        
-        """
+
         if serializer_field.parent.instance:
-            current_table_name = serializer_field.parent.instance.table_name
+            current_table_name = getattr(serializer_field.parent.instance, 'table_name', None)
         else:
             current_table_name = None
-        """
 
         if bool(re.search(r'^[0-9a-zA-Z_\-]+$', table_name)) is False:
             raise ValidationError([self.message_allowed_chars])
